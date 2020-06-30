13e410dbb1f0b1541d2472648f7684905a0ea94c
DerekDardzinski <dardzinski.derek@gmail.com>
2020-06-30 07:22:25 -0700

    Added plotting to specific orbitals and plotting to orbitals on specific atoms

--- src/band_class.py
+++ src/band_class.py
@@ -129,15 +129,40 @@ class BandStructure:
 
         return spd_dict
 
+    def sum_orbital(self, orbitals):
+
+        orbital_dict = {band: np.nan for band in self.projected_dict}
+
+        for band in self.projected_dict:
+            atom_list = [
+                self.projected_dict[band][atom] for atom in self.projected_dict[band]]
+            orbital_dict[band] = reduce(
+                lambda x, y: x.add(y, fill_value=0), atom_list
+            )
+
+        for band in orbital_dict:
+            df = orbital_dict[band]
+            for col in df.columns.tolist():
+                if sum(np.isin(orbitals, col)) == 0:
+                    orbital_dict[band] = orbital_dict[band].drop(columns=col)
+
+        return orbital_dict
+
     def get_kticks(self, ax):
         high_sym_points = self.kpoints.kpts
         kpts_labels = np.array([f'${k}$' for k in self.kpoints.labels])
         all_kpoints = self.vasprun.actual_kpoints
 
-        index = []
-        for i in range(len(high_sym_points)):
-            if high_sym_points[i] != high_sym_points[i - 1]:
+        # index = []
+        # for i in range(len(high_sym_points)):
+        # if high_sym_points[i] != high_sym_points[i - 1]:
+        # index.append(i)
+
+        index = [0]
+        for i in range(len(high_sym_points) - 2):
+            if high_sym_points[i + 2] != high_sym_points[i + 1]:
                 index.append(i)
+        index.append(len(high_sym_points) - 1)
 
         kpts_loc = np.isin(all_kpoints, high_sym_points).all(1)
         kpoints_index = np.where(kpts_loc == True)[0]
@@ -194,22 +219,86 @@ class BandStructure:
                 zorder=1,
             )
 
+        pass
+
+    def plot_atom_orbitals(self, atom_orbital_pairs, ax, scale_factor=5, colors=None):
+        self.plot_plain(ax=ax, linewidth=0.75)
+        self.get_kticks(ax=ax)
+
+        projected_dict = self.projected_dict
+        wave_vector = range(len(self.bands_dict['band1']))
+
+        if colors is None:
+            colors = ['red', 'blue', 'green', 'orange', 'purple']
+
+        for band in projected_dict:
+            for i, atom_orbital_pair in enumerate(atom_orbital_pairs):
+                atom = atom_orbital_pair[0]
+                orbital = atom_orbital_pair[1]
+
+                ax.scatter(
+                    wave_vector,
+                    self.bands_dict[band],
+                    c=colors[i],
+                    s=scale_factor * projected_dict[band][atom][orbital]
+                )
+
+        pass
+
+    def compare_orbitals(self, orbitals, ax, scale_factor=5, color_dict=None):
+        self.plot_plain(ax=ax, linewidth=0.75)
+        self.get_kticks(ax=ax)
+
+        orbital_dict = self.sum_orbital(orbitals=orbitals)
+
+        if color_dict is None:
+            color_dict = {
+                0: 'red',
+                1: 'green',
+                2: 'blue',
+                3: 'orange',
+                4: 'purple',
+                5: 'gold',
+                6: 'mediumturquoise',
+                7: 'navy',
+                8: 'springgreen',
+            }
+
+        plot_df = pd.DataFrame(columns=['s', 'p', 'd'])
+        plot_band = []
+        plot_wave_vec = []
+
+        for band in orbital_dict:
+            plot_df = plot_df.append(orbital_dict[band])
+            plot_band.extend(self.bands_dict[band])
+            plot_wave_vec.extend(range(len(orbital_dict[band])))
+
+        for orbital in orbitals:
+            ax.scatter(
+                plot_wave_vec,
+                plot_band,
+                c=color_dict[orbital],
+                s=scale_factor * plot_df[orbital],
+                zorder=1,
+            )
+
+        pass
+
 
 def main():
-    bands = BandStructure(folder='../Fe', projected=True, spin='up')
-    s = time.time()
+    bands = BandStructure(folder='../../vaspvis_data/band',
+                          projected=True, spin='up')
     fig = plt.figure(figsize=(4, 3), dpi=300)
     ax = fig.add_subplot(111)
     # bands.plot_spd(ax=ax, order=['d', 'p', 's'])
-    bands.plot_plain(ax=ax)
+    # bands.plot_plain(ax=ax)
+    # bands.plot_atom_orbitals(ax=ax, atom_orbital_pairs=[[0, 0], [1, 3]])
+    # bands.compare_orbitals(ax=ax, orbitals=[0, 1, 2, 3, 4, 5, 6, 7, 8])
     plt.ylim(-6, 6)
-    e = time.time()
-    print(e-s)
     plt.ylabel('$E - E_F$ $(eV)$', fontsize=6)
     plt.tick_params(labelsize=6, length=1.5)
     plt.tick_params(axis='x', length=0)
     plt.tight_layout(pad=0.5)
-    plt.savefig('plain_bs_Fe.png')
     plt.show()
 
 
