ae43ebeee3aa56d7aef51b102228bdc2b438a76d
DerekDardzinski <dardzinski.derek@gmail.com>
2020-06-30 09:36:16 -0700

    Removing old files

--- src/band.py
+++ src/band.py
@@ -1,339 +1,306 @@
 from pymatgen.electronic_structure.core import Spin, Orbital
+from pymatgen.io.vasp.outputs import BSVasprun
+from pymatgen.io.vasp.inputs import Kpoints, Poscar
+from functools import reduce
 import matplotlib.pyplot as plt
 import numpy as np
-import json
 import pandas as pd
+import time
 
 
-def get_bands(eigenvalues, efermi):
-
-    bands_dict = {f'band{i+1}': [] for i in range(len(eigenvalues[0]))}
-
-    for kpoint in eigenvalues:
-        for (i, eigenvalue) in enumerate(kpoint):
-            bands_dict[f'band{i+1}'].append(eigenvalue[0] - efermi)
-
-    return bands_dict
-
-
-def get_orbital_atom_weights(projected_eigenvalues, orbitals, atoms):
-
-    orbital_atom_dict = {}
-
-    for i in range(len(projected_eigenvalues[0])):
-        orbital_atom_dict[f'band{i+1}'] = {
-            atom: {orbital: [] for orbital in orbitals} for atom in atoms
-        }
-
-    for kpoint in projected_eigenvalues:
-        for (j, projected_eigenvalue) in enumerate(kpoint):
-            band = f'band{j+1}'
-            for atom in atoms:
-                for orbital in orbitals:
-                    orbital_atom_dict[band][atom][orbital].append(
-                        projected_eigenvalue[atom][orbital]
-                    )
-    
-    return orbital_atom_dict
-
-
-def get_orbital_weights(projected_eigenvalues, orbitals):
-
-    orbitals_dict = {}
-
-    for i in range(len(projected_eigenvalues[0])):
-        orbitals_dict[f'band{i+1}'] = {orbital: [] for orbital in orbitals}
-
-    for kpoint in projected_eigenvalues:
-        for (j, projected_eigenvalue) in enumerate(kpoint):
-            band = f'band{j+1}'
-            atom_sums = np.sum(projected_eigenvalue, axis=0).tolist()
-            for orbital in orbitals:
-                orbitals_dict[band][orbital].append(
-                    atom_sums[orbital]
-                )
-
-    return orbitals_dict
-
-
-def get_atom_weights(projected_eigenvalues, atomic_symbol):
-
-    atoms_dict = {}
-
-    for i in range(len(projected_eigenvalues[0])):
-        atoms_dict[f'band{i+1}'] = {atom: [] for atom in atomic_symbol}
-
-    for kpoint in projected_eigenvalues:
-        for (j, projected_eigenvalue) in enumerate(kpoint):
-            band = f'band{j+1}'
-            orbital_sums = np.sum(projected_eigenvalue, axis=1).tolist()
-            for atom in atomic_symbol:
-                atoms_dict[band][atom].append(
-                    orbital_sums[atom]
-                )
-
-    return atoms_dict
-
-
-def plot_bands(wave_vector, bands_dict, ax, color='black', linewidth=1.5, style='-', alpha=1):
-    for band in bands_dict:
-        ax.plot(
-            wave_vector,
-            bands_dict[band],
-            color=color,
-            linestyle=style,
-            linewidth=linewidth,
-            alpha=alpha,
-            zorder=0,
+class BandStructure:
+    def __init__(self, folder, projected=False, hse=False, spin='up'):
+        self.vasprun = BSVasprun(
+            f'{folder}/vasprun.xml',
+            parse_projected_eigen=projected
         )
+        self.projected = projected
+        self.hse = hse
+        self.folder = folder
+        self.spin = 'up'
+        self.bands_dict = self.load_bands()
 
+        if projected:
+            self.projected_dict = self.load_projected_bands_matrix()
 
-def plot_orbital_atom(wave_vector, bands_dict, orbital_atom_dict, ax, alpha=0.6, scale_factor=30):
-    colors_dict = {
-        0: 'red',
-        1: 'blue',
-        2: 'blue',
-        3: 'blue',
-        4: 'green',
-        5: 'green',
-        6: 'green',
-        7: 'green',
-        8: 'green',
-    }
+        if not hse:
+            self.kpoints = Kpoints.from_file(f'{folder}/KPOINTS')
 
-    df = pd.DataFrame.from_dict(orbital_atom_dict['band10'][0])
-    df.to_csv('test.txt', sep='\t')
+    def load_bands(self):
+        eigenvalues = self.vasprun.eigenvalues
+        efermi = self.vasprun.efermi
+        spin = self.spin
+        nkpoints = len(eigenvalues[Spin.up])
+        nbands = len(eigenvalues[Spin.up][0])
+        spin_dict = {'up': Spin.up, 'down': Spin.down}
 
-    for band in bands_dict:
-        for atom in orbital_atom_dict[band]:
-            for orbital in orbital_atom_dict[band][atom]:
+        bands_dict = {f'band{i+1}': [] for i in range(nbands)}
+
+        for i in range(nkpoints):
+            for j in range(nbands):
+                bands_dict[f'band{j+1}'].append(
+                    eigenvalues[spin_dict[spin]][i][j][0] - efermi
+                )
+
+        return bands_dict
+
+    def load_projected_bands(self):
+        assert_message = "Projected eigenvalues not loaded pass projected=True to load them."
+        assert self.projected is True, assert_message
+
+        projected_eigenvalues = self.vasprun.projected_eigenvalues
+        poscar = Poscar.from_file(
+            f'{self.folder}/POSCAR',
+            check_for_POTCAR=False,
+            read_velocities=False
+        )
+        natoms = len(poscar.site_symbols)
+        nkpoints = len(projected_eigenvalues[Spin.up])
+        nbands = len(projected_eigenvalues[Spin.up][0])
+
+        projected_dict = {}
+
+        for i in range(nbands):
+            projected_dict[f'band{i+1}'] = \
+                {atom: {orbital: {}
+                        for orbital in range(9)} for atom in range(natoms)}
+
+        for i in range(nkpoints):
+            for j in range(nbands):
+                band = f'band{j+1}'
+                for atom in range(natoms):
+                    for orbital in range(9):
+                        projected_dict[band][atom][orbital][i] = \
+                            projected_eigenvalues[Spin.up][i][j][atom][orbital]
+
+        return projected_dict
+
+    def load_projected_bands_matrix(self):
+        projected_eigenvalues = self.vasprun.projected_eigenvalues
+        poscar = Poscar.from_file(
+            f'{self.folder}/POSCAR',
+            check_for_POTCAR=False,
+            read_velocities=False
+        )
+        natoms = len(poscar.site_symbols)
+        nkpoints = len(projected_eigenvalues[Spin.up])
+        nbands = len(projected_eigenvalues[Spin.up][0])
+
+        projected_dict = {f'band{i+1}':
+                          {atom: np.zeros(9) for atom in range(natoms)}
+                          for i in range(nbands)}
+
+        for i in range(nkpoints):
+            for j in range(nbands):
+                band = f'band{j+1}'
+                for atom in range(natoms):
+                    orbital_weights = projected_eigenvalues[Spin.up][i][j][atom]
+                    projected_dict[band][atom] = np.vstack([
+                        projected_dict[band][atom],
+                        orbital_weights
+                    ])
+
+        for band in projected_dict:
+            for atom in projected_dict[band]:
+                projected_dict[band][atom] = pd.DataFrame(
+                    projected_dict[band][atom][1:]
+                )
+
+        return projected_dict
+
+    def sum_spd(self):
+        spd_orbitals = {'s': [0], 'p': [1, 2, 3], 'd': [4, 5, 6, 7, 8]}
+
+        spd_dict = {band: np.nan for band in self.projected_dict}
+
+        for band in self.projected_dict:
+            atom_list = [
+                self.projected_dict[band][atom] for atom in self.projected_dict[band]]
+            spd_dict[band] = reduce(
+                lambda x, y: x.add(y, fill_value=0), atom_list
+            )
+
+        for band in spd_dict:
+            df = spd_dict[band]
+            spd_dict[band]['s'] = df[0]
+            spd_dict[band]['p'] = df[1] + df[2] + df[3]
+            spd_dict[band]['d'] = df[4] + df[5] + df[6] + df[7] + df[8]
+            spd_dict[band] = spd_dict[band].drop(columns=range(9))
+
+        return spd_dict
+
+    def sum_orbital(self, orbitals):
+
+        orbital_dict = {band: np.nan for band in self.projected_dict}
+
+        for band in self.projected_dict:
+            atom_list = [
+                self.projected_dict[band][atom] for atom in self.projected_dict[band]]
+            orbital_dict[band] = reduce(
+                lambda x, y: x.add(y, fill_value=0), atom_list
+            )
+
+        for band in orbital_dict:
+            df = orbital_dict[band]
+            for col in df.columns.tolist():
+                if sum(np.isin(orbitals, col)) == 0:
+                    orbital_dict[band] = orbital_dict[band].drop(columns=col)
+
+        return orbital_dict
+
+    def get_kticks(self, ax):
+        high_sym_points = self.kpoints.kpts
+        kpts_labels = np.array([f'${k}$' for k in self.kpoints.labels])
+        all_kpoints = self.vasprun.actual_kpoints
+
+        # index = []
+        # for i in range(len(high_sym_points)):
+        # if high_sym_points[i] != high_sym_points[i - 1]:
+        # index.append(i)
+
+        index = [0]
+        for i in range(len(high_sym_points) - 2):
+            if high_sym_points[i + 2] != high_sym_points[i + 1]:
+                index.append(i)
+        index.append(len(high_sym_points) - 1)
+
+        kpts_loc = np.isin(all_kpoints, high_sym_points).all(1)
+        kpoints_index = np.where(kpts_loc == True)[0]
+
+        kpts_labels = kpts_labels[index]
+        kpoints_index = list(kpoints_index[index])
+
+        for i in range(len(kpoints_index)):
+            if 0 < i < len(kpoints_index) - 1:
+                kpoints_index[i] = kpoints_index[i] + 0.5
+
+        for k in kpoints_index:
+            ax.axvline(x=k, color='black', alpha=0.7, linewidth=0.5)
+
+        plt.xticks(kpoints_index, kpts_labels)
+
+    def plot_plain(self, ax, color='black', linewidth=1.5):
+        wave_vector = range(len(self.bands_dict['band1']))
+
+        for band in self.bands_dict:
+            band_values = self.bands_dict[band]
+            ax.plot(
+                wave_vector,
+                band_values,
+                color=color,
+                linewidth=linewidth,
+                zorder=0,
+            )
+
+        self.get_kticks(ax=ax)
+        plt.xlim(0, len(wave_vector)-1)
+
+    def plot_spd(self, ax, scale_factor=5, order=['s', 'p', 'd']):
+        spd_dict = self.sum_spd()
+        color_dict = {'s': 'red', 'p': 'blue', 'd': 'green'}
+
+        self.plot_plain(ax, linewidth=0.5)
+
+        plot_df = pd.DataFrame(columns=['s', 'p', 'd'])
+        plot_band = []
+        plot_wave_vec = []
+
+        for band in spd_dict:
+            plot_df = plot_df.append(spd_dict[band])
+            plot_band.extend(self.bands_dict[band])
+            plot_wave_vec.extend(range(len(spd_dict[band])))
+
+        for col in order:
+            ax.scatter(
+                plot_wave_vec,
+                plot_band,
+                c=color_dict[col],
+                s=scale_factor * plot_df[col],
+                zorder=1,
+            )
+
+        pass
+
+    def plot_atom_orbitals(self, atom_orbital_pairs, ax, scale_factor=5, colors=None):
+        self.plot_plain(ax=ax, linewidth=0.75)
+        self.get_kticks(ax=ax)
+
+        projected_dict = self.projected_dict
+        wave_vector = range(len(self.bands_dict['band1']))
+
+        if colors is None:
+            colors = ['red', 'blue', 'green', 'orange', 'purple']
+
+        for band in projected_dict:
+            for i, atom_orbital_pair in enumerate(atom_orbital_pairs):
+                atom = atom_orbital_pair[0]
+                orbital = atom_orbital_pair[1]
+
                 ax.scatter(
                     wave_vector,
-                    bands_dict[band],
-                    c=colors_dict[orbital],
-                    s=scale_factor *
-                    np.array(orbital_atom_dict[band][atom][orbital]),
-                    zorder=1,
-                    alpha=alpha,
-                    edgecolors=None,
+                    self.bands_dict[band],
+                    c=colors[i],
+                    s=scale_factor * projected_dict[band][atom][orbital]
                 )
 
+        pass
 
-def plot_orbital(wave_vector, bands_dict, orbitals_dict, ax, alpha=0.6, scale_factor=30):
-    colors_dict = {
-        0: 'red',
-        1: 'blue',
-        2: 'blue',
-        3: 'blue',
-        4: 'green',
-        5: 'green',
-        6: 'green',
-        7: 'green',
-        8: 'green',
-    }
-    
-    print(len(list(bands_dict.keys())))
-    for band in bands_dict:
-        for orbital in orbitals_dict[band]:
+    def compare_orbitals(self, orbitals, ax, scale_factor=5, color_dict=None):
+        self.plot_plain(ax=ax, linewidth=0.75)
+        self.get_kticks(ax=ax)
+
+        orbital_dict = self.sum_orbital(orbitals=orbitals)
+
+        if color_dict is None:
+            color_dict = {
+                0: 'red',
+                1: 'green',
+                2: 'blue',
+                3: 'orange',
+                4: 'purple',
+                5: 'gold',
+                6: 'mediumturquoise',
+                7: 'navy',
+                8: 'springgreen',
+            }
+
+        plot_df = pd.DataFrame(columns=['s', 'p', 'd'])
+        plot_band = []
+        plot_wave_vec = []
+
+        for band in orbital_dict:
+            plot_df = plot_df.append(orbital_dict[band])
+            plot_band.extend(self.bands_dict[band])
+            plot_wave_vec.extend(range(len(orbital_dict[band])))
+
+        for orbital in orbitals:
             ax.scatter(
-                wave_vector,
-                bands_dict[band],
-                c=colors_dict[orbital],
-                s=scale_factor*np.array(orbitals_dict[band][orbital]),
+                plot_wave_vec,
+                plot_band,
+                c=color_dict[orbital],
+                s=scale_factor * plot_df[orbital],
                 zorder=1,
-                alpha=alpha,
-                edgecolors=None,
             )
 
-
-def plot_atom(wave_vector, bands_dict, atoms_dict, ax, alpha=0.6, scale_factor=30):
-    for band in bands_dict:
-        for atom in atoms_dict[band]:
-            ax.scatter(
-                wave_vector,
-                bands_dict[band],
-                c='blue',
-                s=scale_factor*np.array(atoms_dict[band][atom]),
-                zorder=1,
-                alpha=alpha,
-                edgecolors=None,
-            )
+        pass
 
 
-def get_kpoints(kpoints, all_kpoints, ax):
-    high_sym_points = kpoints.kpts
-    kpts_labels = np.array([f'${k}$' for k in kpoints.labels])
-
-    index = [0]
-    for i in range(len(high_sym_points) - 2):
-        if high_sym_points[i + 2] != high_sym_points[i + 1]:
-            index.append(i)
-    index.append(len(high_sym_points) - 1)
-
-    kpts_loc = np.isin(all_kpoints, high_sym_points).all(1)
-    kpoints_index = np.where(kpts_loc == True)[0]
-
-    kpts_labels = kpts_labels[index]
-    kpoints_index = list(kpoints_index[index])
-
-    for i in range(len(kpoints_index)):
-        if 0 < i < len(kpoints_index) - 1:
-            kpoints_index[i] = kpoints_index[i] + 0.5
-
-    for k in kpoints_index:
-        ax.axvline(x=k, color='black', alpha=0.7, linewidth=0.75)
-
-    plt.xticks(kpoints_index, kpts_labels)
+def main():
+    bands = BandStructure(folder='../../vaspvis_data/band',
+                          projected=True, spin='up')
+    fig = plt.figure(figsize=(4, 3), dpi=300)
+    ax = fig.add_subplot(111)
+    # bands.plot_spd(ax=ax, order=['d', 'p', 's'])
+    # bands.plot_plain(ax=ax)
+    # bands.plot_atom_orbitals(ax=ax, atom_orbital_pairs=[[0, 0], [1, 3]])
+    # bands.compare_orbitals(ax=ax, orbitals=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    plt.ylim(-6, 6)
+    plt.ylabel('$E - E_F$ $(eV)$', fontsize=6)
+    plt.tick_params(labelsize=6, length=1.5)
+    plt.tick_params(axis='x', length=0)
+    plt.tight_layout(pad=0.5)
+    plt.show()
 
 
-def orbital_atom_band(vasprun, kpoints, orbitals, atoms, ax, spin=Spin.up):
-    eigenvalues = vasprun.eigenvalues[spin]
-
-    projected_eigenvalues = vasprun.projected_eigenvalues[spin]
-
-    efermi = vasprun.efermi
-
-    atomic_symbol = vasprun.atomic_symbols
-
-    all_kpoints = np.array(vasprun.actual_kpoints)
-
-    bands_dict = get_bands(eigenvalues, efermi)
-
-    orbital_atom_dict = get_orbital_atom_weights(
-        projected_eigenvalues,
-        orbitals,
-        atoms
-    )
-
-    plt.xlim(0, len(all_kpoints) - 1)
-    wave_vector = range(len(all_kpoints))
-
-    plot_bands(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        ax=ax,
-        alpha=0.5,
-        linewidth=1
-    )
-
-    plot_orbital_atom(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        orbital_atom_dict=orbital_atom_dict,
-        ax=ax,
-        alpha=1,
-        scale_factor=5
-    )
-
-    get_kpoints(
-        kpoints=kpoints,
-        all_kpoints=all_kpoints,
-        ax=ax,
-    )
-
-
-def orbital_band(vasprun, kpoints, orbitals, ax, spin=Spin.up):
-    eigenvalues = vasprun.eigenvalues[spin]
-
-    projected_eigenvalues = vasprun.projected_eigenvalues[spin]
-
-    efermi = vasprun.efermi
-
-    atomic_symbol = vasprun.atomic_symbols
-
-    all_kpoints = np.array(vasprun.actual_kpoints)
-
-    bands_dict = get_bands(eigenvalues, efermi)
-
-    orbitals_dict = get_orbital_weights(projected_eigenvalues, orbitals)
-
-    plt.xlim(0, len(all_kpoints) - 1)
-    wave_vector = range(len(all_kpoints))
-
-    plot_bands(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        ax=ax,
-        alpha=0.5,
-        linewidth=1
-    )
-
-    plot_orbital(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        orbitals_dict=orbitals_dict,
-        ax=ax,
-        alpha=1,
-        scale_factor=5
-    )
-
-    get_kpoints(
-        kpoints=kpoints,
-        all_kpoints=all_kpoints,
-        ax=ax,
-    )
-
-
-def atom_band(vasprun, kpoints, atoms, ax, spin=Spin.up):
-    eigenvalues = vasprun.eigenvalues[spin]
-
-    projected_eigenvalues = vasprun.projected_eigenvalues[spin]
-
-    efermi = vasprun.efermi
-
-    atomic_symbol = vasprun.atomic_symbols
-
-    all_kpoints = np.array(vasprun.actual_kpoints)
-
-    bands_dict = get_bands(eigenvalues, efermi)
-
-    atoms_dict = get_atom_weights(projected_eigenvalues, atoms)
-
-    plt.xlim(0, len(all_kpoints) - 1)
-    wave_vector = range(len(all_kpoints))
-
-    plot_bands(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        ax=ax,
-        alpha=0.5,
-        linewidth=1
-    )
-
-    plot_atom(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        atoms_dict=atoms_dict,
-        ax=ax,
-        alpha=1,
-        scale_factor=5
-    )
-
-    get_kpoints(kpoints, all_kpoints, ax)
-
-
-def plain_band(vasprun, kpoints, ax, spin=Spin.up):
-    eigenvalues = vasprun.eigenvalues[spin]
-
-    efermi = vasprun.efermi
-
-    atomic_symbol = vasprun.atomic_symbols
-
-    all_kpoints = np.array(vasprun.actual_kpoints)
-
-    bands_dict = get_bands(eigenvalues, efermi)
-
-    plt.xlim(0, len(all_kpoints) - 1)
-    wave_vector = range(len(all_kpoints))
-
-    plot_bands(
-        wave_vector=wave_vector,
-        bands_dict=bands_dict,
-        ax=ax,
-    )
-
-    get_kpoints(
-        kpoints=kpoints,
-        all_kpoints=all_kpoints,
-        ax=ax,
-    )
+if __name__ == "__main__":
+    main()
