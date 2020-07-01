158d67e4ad47c1447002cfd0ed0ff4167b83e80a
DerekDardzinski <dardzinski.derek@gmail.com>
2020-06-30 20:42:24 -0700

    Added doc strings to each function

--- src/band.py
+++ src/band.py
@@ -9,7 +9,28 @@ import time
 
 
 class BandStructure:
+    """
+    This class contains all the methods for constructing band structures
+    from the outputs of VASP band structure calculations.
+    """
+
     def __init__(self, folder, projected=False, hse=False, spin='up'):
+        """
+        Initialize parameters upon the generation of this class
+
+        Inputs:
+        ----------
+        folder: (str) This is the folder that contains the VASP files
+        projected: (bool) Determined wheter of not to parte the projected
+            eigenvalues from the vasprun.xml file. Making this true
+            increases the computational time, so only use if a projected
+            band structure is required.
+        hse: (bool) Determines if the KPOINTS file is in the form of HSE
+            or not. Only make true if the band structure was calculated
+            using a hybrid functional.
+        spin: (str) Choose which spin direction to parse. ('up' or 'down')
+        """
+
         self.vasprun = BSVasprun(
             f'{folder}/vasprun.xml',
             parse_projected_eigen=projected
@@ -21,12 +42,23 @@ class BandStructure:
         self.bands_dict = self.load_bands()
 
         if projected:
-            self.projected_dict = self.load_projected_bands_matrix()
+            self.projected_dict = self.load_projected_bands()
 
         if not hse:
             self.kpoints = Kpoints.from_file(f'{folder}/KPOINTS')
 
     def load_bands(self):
+        """
+        This function is used to load eigenvalues from the vasprun.xml
+        file and into a dictionary which is in the form of
+        band index --> eigenvalues
+
+        Output:
+        ---------
+        bands_dict: (dict[str][np.ndarray]) Dictionary which contains
+            the eigenvalues for each band
+        """
+
         eigenvalues = self.vasprun.eigenvalues
         efermi = self.vasprun.efermi
         spin = self.spin
@@ -44,38 +76,19 @@ class BandStructure:
 
         return bands_dict
 
+
     def load_projected_bands(self):
-        assert_message = "Projected eigenvalues not loaded pass projected=True to load them."
-        assert self.projected is True, assert_message
+        """
+        This function loads the project weights of the orbitals in each band
+        from vasprun.xml into a dictionary of the form:
+        band index --> atom index --> weights of orbitals
 
-        projected_eigenvalues = self.vasprun.projected_eigenvalues
-        poscar = Poscar.from_file(
-            f'{self.folder}/POSCAR',
-            check_for_POTCAR=False,
-            read_velocities=False
-        )
-        natoms = len(poscar.site_symbols)
-        nkpoints = len(projected_eigenvalues[Spin.up])
-        nbands = len(projected_eigenvalues[Spin.up][0])
+        Output:
+        ----------
+        projected_dict: (dict([str][int][pd.DataFrame])) Dictionary containing the
+            projected weights of all orbitals on each atom for each band.
+        """
 
-        projected_dict = {}
-
-        for i in range(nbands):
-            projected_dict[f'band{i+1}'] = \
-                {atom: {orbital: {}
-                        for orbital in range(9)} for atom in range(natoms)}
-
-        for i in range(nkpoints):
-            for j in range(nbands):
-                band = f'band{j+1}'
-                for atom in range(natoms):
-                    for orbital in range(9):
-                        projected_dict[band][atom][orbital][i] = \
-                            projected_eigenvalues[Spin.up][i][j][atom][orbital]
-
-        return projected_dict
-
-    def load_projected_bands_matrix(self):
         projected_eigenvalues = self.vasprun.projected_eigenvalues
         poscar = Poscar.from_file(
             f'{self.folder}/POSCAR',
@@ -109,6 +122,17 @@ class BandStructure:
         return projected_dict
 
     def sum_spd(self):
+        """
+        This function sums the weights of the s, p, and d orbitals for each atom
+        and creates a dictionary of the form:
+        band index --> s,p,d orbital weights
+
+        Outputs:
+        ----------
+        spd_dict: (dict([str][pd.DataFrame])) Dictionary that contains the summed
+            weights for the s, p, and d orbitals for each band
+        """
+
         spd_orbitals = {'s': [0], 'p': [1, 2, 3], 'd': [4, 5, 6, 7, 8]}
 
         spd_dict = {band: np.nan for band in self.projected_dict}
@@ -130,6 +154,29 @@ class BandStructure:
         return spd_dict
 
     def sum_orbital(self, orbitals):
+        """
+        This function finds the weights of desired orbitals for all atoms and
+            returns a dictionary of the form:
+            band index --> orbital index
+
+        Inputs:
+        ----------
+        orbitals: (list) List of desired orbitals. 
+            0 = s
+            1 = px
+            2 = py
+            3 = pz
+            4 = dxx
+            5 = dxy
+            6 = dxz
+            7 = dyz
+            8 = dzz
+
+        Outputs:
+        ----------
+        orbital_dict: (dict[str][pd.DataFrame]) Dictionary that contains the projected
+            weights of the selected orbitals.
+        """
 
         orbital_dict = {band: np.nan for band in self.projected_dict}
 
@@ -149,14 +196,18 @@ class BandStructure:
         return orbital_dict
 
     def get_kticks(self, ax):
+        """
+        This function extracts the kpoint labels and index locations for a regular
+        band structure calculation (non HSE).
+
+        Inputs:
+        ----------
+        ax: (matplotlib.pyplot.axis) Axis to append the tick labels
+        """
+
         high_sym_points = self.kpoints.kpts
         kpts_labels = np.array([f'${k}$' for k in self.kpoints.labels])
         all_kpoints = self.vasprun.actual_kpoints
-
-        # index = []
-        # for i in range(len(high_sym_points)):
-        # if high_sym_points[i] != high_sym_points[i - 1]:
-        # index.append(i)
 
         index = [0]
         for i in range(len(high_sym_points) - 2):
@@ -180,6 +231,17 @@ class BandStructure:
         plt.xticks(kpoints_index, kpts_labels)
 
     def plot_plain(self, ax, color='black', linewidth=1.5):
+        """
+        This function plots a plain band structure given that the band data
+        has already been loaded with the load_bands() method.
+
+        Inputs:
+        ----------
+        ax: (matplotlib.pyplot.axis) Axis to plot the data on
+        color: (str) Color of the band structure lines
+        linewidth: (float) Line width of the band structure lines
+        """
+
         wave_vector = range(len(self.bands_dict['band1']))
 
         for band in self.bands_dict:
@@ -195,9 +257,32 @@ class BandStructure:
         self.get_kticks(ax=ax)
         plt.xlim(0, len(wave_vector)-1)
 
-    def plot_spd(self, ax, scale_factor=5, order=['s', 'p', 'd']):
+    def plot_spd(self, ax, scale_factor=5, order=['s', 'p', 'd'], color_dict=None):
+        """
+        This function plots the s, p, d projected band structure given that the band
+        data has already been loaded with the load_bands() and load_projected_bands()
+        methods
+
+        Inputs:
+        ----------
+        ax: (matplotlib.pyplot.axis) Axis to plot the data on
+        scale_factor: (float) Factor to scale weights. This changes the size of the
+            points in the scatter plot
+        order: (list) This determines the order in which the points are plotted on the
+            graph. This is an option because sometimes certain orbitals can be hidden
+            under other orbitals because they have a larger weight. For example, if the
+            signitures of the d orbitals are greater than that of the s orbitals, it
+            might be smart to choose ['d', 'p', 's'] as the order so the s orbitals are
+            plotted over the d orbitals.
+        color_dict: (dict[str][str]) This option allow the colors of the s, p, and d
+            orbitals to be specified. Should be in the form of:
+            {'s': <s color>, 'p': <p color>, 'd': <d color>}
+        """
+
         spd_dict = self.sum_spd()
-        color_dict = {'s': 'red', 'p': 'blue', 'd': 'green'}
+
+        if color_dict is None:
+            color_dict = {'s': 'red', 'p': 'blue', 'd': 'green'}
 
         self.plot_plain(ax, linewidth=0.5)
 
@@ -222,6 +307,24 @@ class BandStructure:
         pass
 
     def plot_atom_orbitals(self, atom_orbital_pairs, ax, scale_factor=5, colors=None):
+        """
+        This function plots the projected band structure of individual orbitals on
+        individual atoms given that the band data has already been loaded with the
+        load_bands() and load_projected_bands() methods
+
+        Inputs:
+        -----------
+        atom_orbital_pairs: (list[list]): Selected orbitals on selected atoms to plot.
+            This should take the form of [[atom index, orbital_index], ...]. 
+            To plots the px orbital of the 1st atom and the pz orbital of the 2nd atom
+            in the POSCAR file, the input would be [[0, 1], [1, 3]]
+        ax: (matplotlib.pyplot.axis) Axis to plot the data on
+        scale_factor: (float) Factor to scale weights. This changes the size of the
+            points in the scatter plot
+        colors: (list) List of colors for the atom-orbital pairs in the order that
+            the atom-orbital pairs were given.
+        """
+
         self.plot_plain(ax=ax, linewidth=0.75)
         self.get_kticks(ax=ax)
 
@@ -246,6 +349,21 @@ class BandStructure:
         pass
 
     def compare_orbitals(self, orbitals, ax, scale_factor=5, color_dict=None):
+        """
+        This function plots the projected band structure of given orbitals summed
+        across all atoms given that the band data has already been loaded with the
+        load_bands() and load_projected_bands() methods.
+
+        Inputs:
+        ----------
+        orbitals: (list) List of orbits to compare
+        ax: (matplotlib.pyplot.axis) Axis to plot the data on
+        scale_factor: (float) Factor to scale weights. This changes the size of the
+            points in the scatter plot
+        color_dict: (dict[str][str]) This option allow the colors of each orbital
+            specified. Should be in the form of:
+            {'orbital index': <color>, 'orbital index': <color>, ...}
+        """
         self.plot_plain(ax=ax, linewidth=0.75)
         self.get_kticks(ax=ax)
 
