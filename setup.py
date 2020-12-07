3e7e6fd1f2b5f4f9a812723d791771b2bdaed70f
DerekDardzinski <dardzinski.derek@gmail.com>
2020-12-07 15:14:18 -0500

    Added an option to plot contour maps instead of heat maps for the dos_layers function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as fh:
 
 setup(
     name='vaspvis',
-    version='0.2.24',
+    version='0.2.25',
     description='A highly flexible and customizable library for visualizing electronic structure data from VASP calculations',
     long_description=long_description,
     long_description_content_type='text/markdown',
